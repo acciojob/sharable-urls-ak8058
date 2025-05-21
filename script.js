61d66aff1ec338dde1c893e5807d9ac7328e4a65
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-05-21 09:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+document.getElementById("button").onclick = function () {
+  let name = document.getElementById("name").value.trim();
+  let graduationyear = document.getElementById("year").value.trim();
+  let url = "https://localhost:8080/";
+  if (name && graduationyear) {
+    url +=
+      "?name=" +
+      encodeURIComponent(name) +
+      "&year=" +
+      encodeURIComponent(graduationyear);
+  } else if (name) {
+    url += "?name=" + encodeURIComponent(name);
+  } else if (graduationyear) {
+    url += "&year=" + encodeURIComponent(graduationyear);
+  }
+  document.getElementById("url").textContent = url;
+};
